86d6308cfe378f93d6d06f24bf5314ba313b9e05
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-02-10 13:26:36 +0100

    printf 42 repo that i made

---
+++ printf_42/printf_42/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayouahid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 13:53:32 by ayouahid          #+#    #+#             */
+/*   Updated: 2024/11/29 13:53:34 by ayouahid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_putchar(int c);
+int	ft_printf(const char *format, ...);
+
+#endif // PRINTF_H
